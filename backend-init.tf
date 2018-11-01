3a67c7cf8cfa8cb9d098d1cac126bb7f926ca6af
Diógenes Oliveira <diogenes1oliveira+github@gmail.com>
2018-10-31 22:26:50 -0300

    [infra] minor customization to the backend file

--- backend-init.tf
+++ backend-init.tf
@@ -1,14 +1,20 @@
-provider "aws" {
-  region = "us-east-2" # Ohio
+
+variable "project_name" {
+  description = "Name of the project" # (!)
+  default = "diogenes-pro"
 }
 
 variable "environments" {
   description = "List of the separate environments in the stack"
-  default     = ["development", "production"]
+  default = ["development", "production"]
+}
+
+provider "aws" {
+  region = "us-east-2" # Ohio
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-diogenes-pro"
+  bucket = "terraform-state-${var.project_name}"
 
   versioning {
     enabled = true
@@ -19,15 +25,15 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 
   tags {
-    Description = "S3 bucket for storing Terraform state"
+    Description = "${var.project_name} - Terraform state"
   }
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  count          = "${length(var.environments)}"
-  name           = "terraform-state-lock-diogenes-pro-${element(var.environments, count.index)}"
-  hash_key       = "LockID"
-  read_capacity  = 20
+  count = "${length(var.environments)}"
+  name = "terraform-state-lock-${var.project_name}-${element(var.environments, count.index)}"
+  hash_key = "LockID"
+  read_capacity = 20
   write_capacity = 20
 
   attribute {
@@ -40,6 +46,6 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
   }
 
   tags {
-    Description = "DynamoDB table for storing the lock for Terraform state for the ${element(var.environments, count.index)} environment"
+    Description = "${var.project_name} - Terraform state lock for the ${element(var.environments, count.index)} environment"
   }
 }
