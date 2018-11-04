5cb93e30d7c980a09fbd564706c975dd4c5cf770
Diógenes Oliveira <diogenes1oliveira+github@gmail.com>
2018-11-03 23:18:14 -0300

    [terraform] outputting the revelant information from the network module

--- infra/network/outputs.tf
+++ infra/network/outputs.tf
@@ -1,4 +1,4 @@
-output "vpc-id" {
+output "vpc-main" {
   value = "${aws_vpc.vpc-main.id}"
 }
 
@@ -10,4 +10,18 @@ output "public-subnets" {
     cidr-blocks = "${aws_subnet.public-subnets.*.cidr_block}"
     ipv6-cidr-blocks = "${aws_subnet.public-subnets.*.ipv6_cidr_block}"
   }
+}
+
+output "private-subnets" {
+  value = {
+    ids = "${aws_subnet.private-subnets.*.id}"
+    names = "${aws_subnet.private-subnets.*.tags.Name}"
+    azs = "${aws_subnet.private-subnets.*.availability_zone}"
+    cidr-blocks = "${aws_subnet.private-subnets.*.cidr_block}"
+    ipv6-cidr-blocks = "${aws_subnet.private-subnets.*.ipv6_cidr_block}"
+  }
+}
+
+output "default-sg" {
+  value = "${aws_security_group.default-sg.id}"
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,11 @@
 output "public-subnets" {
   value = "${module.network.public-subnets}"
 }
+
+output "private-subnets" {
+  value = "${module.network.private-subnets}"
+}
+
+output "default-sg" {
+  value = "${module.network.default-sg}"
+}
