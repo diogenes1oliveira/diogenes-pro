0877940d9d2d8479c0aaf1627e80865b258964e8
Diógenes Oliveira <diogenes1oliveira+github@gmail.com>
2018-11-03 22:15:59 -0300

    [terraform] setting up private subnets with IPv6

---
+++ infra/network/private-subnets.tf
@@ -0,0 +1,46 @@
+
+resource "aws_subnet" "private-subnets" {
+  count = "${length(var.vpc-private-subnets)}"
+
+  vpc_id = "${aws_vpc.vpc-main.id}"
+  cidr_block = "${cidrsubnet(aws_vpc.vpc-main.cidr_block, 4, 15 - count.index)}"
+  ipv6_cidr_block = "${cidrsubnet(aws_vpc.vpc-main.ipv6_cidr_block, 8, 255 - count.index)}"
+  availability_zone ="${lookup(var.vpc-private-subnets[count.index], "az")}"
+
+  map_public_ip_on_launch = false
+  assign_ipv6_address_on_creation = false
+
+  tags {
+    Name = "${lookup(var.vpc-private-subnets[count.index], "name")}"
+    Category = "network"
+    Project = "${var.project-name}"
+    Description = "Private subnet in the ${lookup(var.vpc-private-subnets[count.index], "az")} AZ for the project ${var.project-name}"
+  }
+}
+
+resource "aws_route_table" "private-subnets-route-tables" {
+  count = "${length(var.vpc-private-subnets)}"
+  vpc_id = "${aws_vpc.vpc-main.id}"
+
+  tags {
+    Name = "route-table-${lookup(var.vpc-private-subnets[count.index], "name")}"
+    Category = "network"
+    Project = "${var.project-name}"
+    Description = "Route table for the private subnet ${lookup(var.vpc-private-subnets[count.index], "name")} in the ${lookup(var.vpc-private-subnets[count.index], "az")} AZ for the project ${var.project-name}"
+  }
+}
+
+resource "aws_route_table_association" "private-subnet-route-table-associations" {
+  count = "${length(var.vpc-private-subnets)}"
+
+  subnet_id = "${element(aws_subnet.private-subnets.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private-subnets-route-tables.*.id, count.index)}"
+}
+
+resource "aws_route" "private-subnets-ipv6-route-to-egress-only-ig" {
+  count = "${length(var.vpc-private-subnets)}"
+
+  route_table_id = "${element(aws_route_table.private-subnets-route-tables.*.id, count.index)}"
+  egress_only_gateway_id = "${aws_egress_only_internet_gateway.vpc-main-egress-only-ig.id}"
+  destination_ipv6_cidr_block = "::/0"
+}
