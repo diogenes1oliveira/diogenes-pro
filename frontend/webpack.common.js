5dbdc3901d62acb815fe8eb80735947492c91fc4
Diógenes Oliveira <diogenes1oliveira+github@gmail.com>
2018-11-08 22:13:28 -0200

    [webpack] building by hand

---
+++ frontend/webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+        ],
+      },
+      {
+        test: /\.(png|svg|gif|jpe?g)$/,
+        exclude: /node_modules/,
+        use: [
+          'file-loader',
+        ],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.wasm', '.mjs', '.json'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html',
+    }),
+  ]
+};
